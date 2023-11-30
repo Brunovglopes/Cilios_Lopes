9964e3425a616d6bffa73dcce410c21ea94ee719
Bruno Vinicius Gonzaga Lopes <141247756+Brunovglopes@users.noreply.github.com>
2023-11-30 11:10:57 -0300

    Update analycits.js

--- analycits.js
+++ analycits.js
@@ -1,9 +1,8 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y4WHB9S67N"></script>
-<script>
+ async src="https://www.googletagmanager.com/gtag/js?id=G-Y4WHB9S67N"
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-Y4WHB9S67N');
-</script>
